80bdaedc6c2e3c12c79df6996030726f23baaebd
Arshana Thasni TM <148333016+arshana-thasni-tm@users.noreply.github.com>
2024-05-01 21:53:49 +0530

    initial

--- lib/screens/firstpage.dart
+++ lib/screens/firstpage.dart
@@ -9,7 +9,6 @@ class firstPage extends StatelessWidget {
       backgroundColor: Colors.purple,
       appBar: AppBar(
         backgroundColor: Colors.purple,
-
       ),
       body: Center(
         child: Column(
@@ -20,29 +19,32 @@ class firstPage extends StatelessWidget {
               height: 300,
               width: 300,
             ),
-            SizedBox(height: 40), // Adding some space between the image and the button
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to the second page when the button is pressed
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => homeScreen()),
-                );
-              },
-              style: ButtonStyle(
-                minimumSize: MaterialStateProperty.all(Size(double.infinity, 10)), // Increase height to 60
-                backgroundColor: MaterialStateProperty.all(Colors.purpleAccent), // Change this to the desired background color
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20), // Set border radius to 15 for a rounded shape
-                )),
-              ),
-              child: Container(
-                height: 75,
-                // Adjust this height as needed
-                child: Center(
-                  child: Text(
-                    'Start Quiz',
-                    style: TextStyle(color: Colors.white, fontSize: 25),
+            SizedBox(height: 50), // Adding some space between the image and the button
+            SizedBox(
+              width: 200, // Adjust the width as needed
+              child: ElevatedButton(
+                onPressed: () {
+                  // Navigate to the second page when the button is pressed
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => QuestionScreen()),
+                  );
+                },
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(Size(double.infinity, 15)), // Increase height to 60
+                  backgroundColor: MaterialStateProperty.all(Colors.purpleAccent), // Change this to the desired background color
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15), // Set border radius to 15 for a rounded shape
+                  )),
+                ),
+                child: Container(
+                  height: 50,
+                  // Adjust this height as needed
+                  child: Center(
+                    child: Text(
+                      'Start Quiz',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
                   ),
                 ),
               ),
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,16 +1,81 @@
+
 import 'package:flutter/material.dart';
 
-class homeScreen extends StatelessWidget {
+class QuestionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple,
       appBar: AppBar(
-        title: Text('Second Page'),
+        backgroundColor: Colors.purple,
       ),
       body: Center(
-        child: Text(
-          'This is the second page!',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Which plant in our system is know as the Red Plant?',
+              style: TextStyle(fontSize: 25.0,color: Colors.white,fontWeight: FontWeight.bold,),textAlign: TextAlign.center,),
+
+
+            SizedBox(height: 20.0),
+            SizedBox(
+              width: 350.0, // Adjust the width as needed
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle option A
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,side: BorderSide(style: BorderStyle.solid,color: Colors.white)
+                ),
+                child: Text('1. Alexander Fleming',),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              width: 350.0, // Adjust the width as needed
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle option B
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,side: BorderSide(style: BorderStyle.solid,color: Colors.white)
+                ),
+                child: Text('2. Alexander Graham Bell'),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              width: 350.0, // Adjust the width as needed
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle option C
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,side: BorderSide(style: BorderStyle.solid,color: Colors.white)// Button color
+                ),
+                child: Text('3. Thomas Alva Edison'),
+              ),
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              width: 350.0, // Adjust the width as needed
+              child: ElevatedButton(
+                onPressed: () {
+                  // Handle option D
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white,side: BorderSide(style: BorderStyle.solid,color: Colors.white)
+                  // Button color
+                ),
+                child: Text('4. Mars'),
+              ),
+            ),
+          ],
         ),
       ),
     );
